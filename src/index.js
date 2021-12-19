d78ac7e4fa6e6bab28870b1b20848f79ceb90123
be1an <8497715@gmail.com>
2021-12-19 23:51:57 +0500

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import { camelCase } from 'lodash';
+// import { Tooltip, Toast, Popover } from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 console.log(camelCase('Hello World!'));
