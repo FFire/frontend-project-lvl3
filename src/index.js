5b8ae9ff177640bab2e9455b59c5d5d974fd31f0
be1an <8497715@gmail.com>
2021-12-21 01:02:30 +0500

    validation step 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,42 @@
-import { camelCase } from 'lodash';
 // import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import onChange from 'on-change';
+import * as yup from 'yup';
 
-console.log(camelCase('Hello World!'));
+const feedSchema = yup.string()
+  .required('URL is required')
+  .matches(/(https?:\/\/)?([\w-])+\.{1}([a-zA-Z]{2,63})([/\w-]*)*\/?\??([^#\n\r]*)?#?([^\n\r]*)/, 'Ссылка должна быть валидным URL');
+
+const render = (state, elements) => {
+  const { feedback, urlInput } = elements;
+  feedback.textContent = state.error;
+  if (state.error) {
+    urlInput.classList.add('is-invalid');
+  } else {
+    urlInput.classList.remove('is-invalid');
+  }
+};
+
+const elements = {
+  urlInput: document.querySelector('#url-input'),
+  feedback: document.querySelector('.feedback.text-danger'),
+};
+
+const state = {
+  error: '',
+  feeds: [],
+};
+
+const watchedState = onChange(state, (path, value, previousValue, applyData) => {
+  render(state, elements);
+});
+
+const onUpdate = (e) => {
+  watchedState.error = e.target.value;
+  feedSchema.validate(e.target.value).catch((err) => {
+    console.dir(err.message);
+  });
+  // console.log(e.target.value);
+};
+
+elements.urlInput.addEventListener('input', onUpdate);
