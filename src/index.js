8ecaf9cc8dd15e05b22815511088941ca83beb6b
be1an <8497715@gmail.com>
2021-12-23 22:31:58 +0500

    Squashed commit of the following:
    commit 5d3813e37347f396a94776c10e94e7184959c34c
    Author: be1an <8497715@gmail.com>
    Date:   Wed Dec 22 00:40:28 2021 +0500
    
        fix: validation complete
    
    commit 5b8ae9ff177640bab2e9455b59c5d5d974fd31f0
    Author: be1an <8497715@gmail.com>
    Date:   Tue Dec 21 01:02:30 2021 +0500
    
        validation step 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,74 @@
-import { camelCase } from 'lodash';
 // import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import onChange from 'on-change';
+import * as yup from 'yup';
 
-console.log(camelCase('Hello World!'));
+const render = (state, elements) => {
+  const { feedback, urlInput } = elements;
+  const { mode, text } = state.message;
+
+  feedback.textContent = '';
+  urlInput.classList.remove('is-invalid');
+  urlInput.classList.remove('text-success');
+
+  switch (mode) {
+    case 'none':
+      break;
+
+    case 'success':
+      feedback.textContent = text;
+      urlInput.classList.add('text-success');
+      break;
+
+    case 'fail':
+      feedback.textContent = text;
+      urlInput.classList.add('is-invalid');
+      break;
+
+    default:
+      throw new Error(`No such message mode: ${mode}`);
+  }
+};
+
+const elements = {
+  form: document.querySelector('form'),
+  urlInput: document.querySelector('#url-input'),
+  feedback: document.querySelector('.feedback.text-danger'),
+};
+
+const state = {
+  message: { mode: 'none', text: '' }, // mode: ['none', 'success', 'fail']
+  feeds: [],
+};
+
+const watchedState = onChange(state, () => { // path, value, previousValue, applyData
+  render(state, elements);
+});
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const url = formData.get('url');
+  state.feeds.push(url);
+  console.dir(state.feeds);
+};
+
+const onValidate = (e) => {
+  const feedSchema = yup.string()
+    .required('URL is required')
+    .matches(/(https?:\/\/)?([\w-])+\.{1}([a-zA-Z]{2,63})([/\w-]*)*\/?\??([^#\n\r]*)?#?([^\n\r]*)/, 'Ссылка должна быть валидным URL')
+    .notOneOf(state.feeds, 'RSS уже существует');
+
+  feedSchema.validate(e.target.value)
+    .then(() => {
+      watchedState.message.text = '';
+      watchedState.message.mode = 'none';
+    })
+    .catch((err) => {
+      watchedState.message.text = err.message;
+      watchedState.message.mode = 'fail';
+    });
+};
+
+elements.urlInput.addEventListener('input', onValidate);
+elements.form.addEventListener('submit', onSubmit);
